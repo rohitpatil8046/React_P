583e28f127e52db0c0aeb823b38fe3bd84d49aff
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-07-25 19:07:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDom from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
+ReactDom.render(<App />,
+      document.getElementById('root'));
