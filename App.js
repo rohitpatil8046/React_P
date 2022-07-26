3a35ddc2f1dc4883b9d656deb13527c1d2d403f6
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-07-26 19:31:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React , { useState } from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ showPassword, setShowPassword ] = useState(true);
+
+    return(
+        <div className="container">
+          <input 
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => { setUsername(e.target.value)}}
+               
+          />
+             <input 
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => { setPassword(e.target.value)}}
+               
+          />
+          <h2>{username}</h2>
+          <h2>{showPassword ? password : '*'.repeat(password.length)}</h2>
+          <button onClick={(e) => setShowPassword(!showPassword)}>Show/Hide Password</button>
+        </div>
+    )
 }
 
 export default App;
